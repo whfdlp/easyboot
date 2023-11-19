e9b1c9dc384a3217f14ac6ba4e0e9cdb3da2ab63
wanhf <wanhf@servyou.com.cn>
2023-11-20 00:33:04 +0800

    desc

--- src/main/java/whf/easy/boot/service/htmlParse/HtmlParseTest.java
+++ src/main/java/whf/easy/boot/service/htmlParse/HtmlParseTest.java
@@ -14,9 +14,13 @@ public class HtmlParseTest {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://ggzy.hzctc.hangzhou.gov.cn/AfficheShow/Home?AfficheID=24c6fe2c-42fb-490b-b947-fb0e3724dc30&IsInner=0&IsHistory=0&ModuleID=22").get();
         Elements table = doc.select("table");
+        int i = 0;
         for (Element element : table) {
             Elements select = element.select("tr").select("td");
-            System.out.println(select.text());
+            for (Element element1 : select) {
+                System.out.println(i++ +"         "+ element1.text());
+            }
+
         }
 //        for (int i = 0;i<=table.size();i++){
 //            Elements font = table.select("font");
